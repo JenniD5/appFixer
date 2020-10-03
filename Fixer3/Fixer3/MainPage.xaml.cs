c9ed236ee19e77860728de640cd4ff26fac49192
JenniD5 <elizabethduran0105@gmail.com>
2020-10-02 20:46:50 -0700

    Agregar archivos de proyecto.

---
+++ Fixer3/Fixer3/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Fixer3
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //MainPage = new NavigationPage(new login1());
+           
+        }
+
+        private void Inicio_Clicked(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void BtnCrearcuenta_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Fixer3/Fixer3/login1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Fixer3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class login1 : ContentPage
+    {
+        public login1()
+        {
+            InitializeComponent();
+        }
+
+        private void Entrada_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+
+        private void Btncrearnuevacuenta_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Nuevacuenta());
+        }
+    }
+}
